c8bc362b5bb534f3783ff6f8efde1f21143e7463
marcelogomezz <144245602+marcelogomezz@users.noreply.github.com>
2023-09-13 21:39:09 -0400

    initial commit

--- script.js
+++ script.js
@@ -2,52 +2,53 @@ const CALCULAR = document.getElementById('calcular');
 const ERROR = document.getElementById('error');
 const FLU = document.getElementById('flu');
 const MAN = document.getElementById('man');
-
-function calcFlujo(peso){
-    let resto = peso;
-    let flujo = 0;
-
-    if (resto>20){
-        let aux = resto-20;
-        flujo += aux*1;
-        resto -= aux;
-    } 
-
-    if (resto>=10){
-        let aux = resto-10;
-        flujo += aux*2;
-        resto -= aux;
-    }
-    flujo += resto*4;
-    
-    return flujo;
-}
+const MAN2 = document.getElementById('man2');
 
 CALCULAR.addEventListener('click', () => {
-    const DATO = document.getElementById('peso').valueAsNumber
-    //validamos que se cargue un dato:
-    if (DATO > 0 && DATO <=30){
-        ERROR.style.display = 'none'
-        let flujo = calcFlujo(DATO);
-        let mantenimiento = flujo*1.5;
-        FLU.innerHTML = flujo + ' cc/hr';
-        MAN.innerHTML = 'm+m/2 ' + mantenimiento + ' cc/hr';
+    const PESO = parseFloat(document.getElementById('peso').value);
+
+    if (!isNaN(PESO) && PESO > 0) {
+        ERROR.style.display = 'none';
+        let voldiario, mantenimiento;
+
+        if (PESO <= 30) {
+            if (PESO <= 10) {
+                voldiario = PESO * 100;
+            } else if (PESO <= 20) {
+                voldiario = (10 * 100) + ((PESO - 10) * 50);
+            } else {
+                voldiario = (10 * 100) + (10 * 50) + ((PESO - 20) * 20);
+            }
+
+            mantenimiento = voldiario / 24;
+        } else {
+            let supcorporal = ((PESO * 4) + 7) / (PESO + 90);
+
+            let decision_medico = 1500; 
+            voldiario = supcorporal * decision_medico;
+
+            mantenimiento = voldiario / 24;
+        }
+
+        let mm_2 = mantenimiento * 1.5; 
+
+
+     voldiario = Math.round(voldiario);
+     mantenimiento = Math.round(mantenimiento);
+     mm_2 = Math.round(mm_2);
+
+        
+        FLU.innerHTML = 'Diario: ' + voldiario + ' cc';
+        MAN.innerHTML = 'Man: ' + mantenimiento + ' cc/hr';
+        MAN2.innerHTML = 'M+m/2: ' + mm_2 + ' cc/hr'; 
+        ERROR.style.display = 'none';
         FLU.style.display = 'block';
         MAN.style.display = 'block';
-    }else if (DATO>30){
-        let SuperficieCorporal = ( (DATO * 4) + 7) / (DATO + 90);
-        FLU.innerHTML = SuperficieCorporal*1500 + ' cc/hr';
-        MAN.innerHTML = SuperficieCorporal*2000 + ' cc/hr';
-        FLU.style.display = 'block';
-        MAN.style.display = 'block';
-    }
-     else {
-    
+        MAN2.style.display = 'block'; 
+    } else {
         ERROR.style.display = 'block';
         FLU.style.display = 'none';
         MAN.style.display = 'none';
+        MAN2.style.display = 'none'; 
     }
-})
-
-
-
+});
